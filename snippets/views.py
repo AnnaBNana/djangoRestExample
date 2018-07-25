d152c8411e6342e167e77bf62f93c11ae9ae8f5b
Anna Propas <anna@Annas-MacBook-Pro.local>
2018-07-25 15:02:28 -0700

    completes basic functionality for all actions on a single resource with API view

--- snippets/views.py
+++ snippets/views.py
@@ -1,50 +1,19 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework import mixins, generics
 
-@csrf_exempt
-def snippet_list(request):
+
+class SnippetList(generics.ListCreateAPIView):
     """
-    List all code snippets, or create a new one
+    List all snippets or create a new snippet
     """
-    if request.method == "GET":
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
 
-    elif request.method == "POST":
-        data = JsonParser().parse(request)
-        serializer = SnippetSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.data, status=400)
 
-@csrf_exempt
-def snippet_detail(request, pk):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a snippet instance.
     """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
